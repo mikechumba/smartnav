079c73ce3c6a3418e40c79ef780a97bfe9e93b92
Michael Kipchumba <michaelchumba09@gmail.com>
2019-06-10 01:01:58 +0300

    Updates

---
+++ precache-manifest.ab250138fdadccbd4ebd1e1ebf72ee65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "286416f345f0bc5d95864cb8306cf9ee",
+    "url": "smartnav.netlify.com/index.html"
+  },
+  {
+    "revision": "652db4ef21f0b54316b6",
+    "url": "smartnav.netlify.com/static/css/main.27610e02.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "smartnav.netlify.com/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "652db4ef21f0b54316b6",
+    "url": "smartnav.netlify.com/static/js/main.a1e6f3b5.chunk.js"
+  },
+  {
+    "revision": "a2972673fd528b86cd76",
+    "url": "smartnav.netlify.com/static/js/runtime~main.b3d069e7.js"
+  },
+  {
+    "revision": "46862a1d919183ea756ae2bf6202c2ec",
+    "url": "smartnav.netlify.com/static/media/cane_handle.46862a1d.png"
+  }
+]);
